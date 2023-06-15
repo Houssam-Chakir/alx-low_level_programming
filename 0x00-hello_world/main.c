790a69fa8c71575bd07fb8826cc613823cc23948
Houssam-Chakir <thisischchannel@gmail.com>
2023-06-15 14:14:51 -0700

    used -E option with gcc to run the source file through the preprocessor

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -7,12 +7,5 @@
  */
 int main(void)
 {
-	printf("# 942 \"/usr/include/stdio.h\" 3 4\n\n");
-	printf("# 2 \"main.c\" 2\n\n");
-	printf("# 3 \"main.c\"\n");
-	printf("int main(void)");
-	printf("{");
-	printf(" return (0);");
-	printf("{");
 	return (0);
 }
