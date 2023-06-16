20815c4f2298e617c580ed39f44342b975a43d50
Houssam <thisischchannel@gmail.com>
2023-06-16 16:37:43 -0700

    wrote and tested task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: using loop to print base 10
+ * single digits until 9
+ * Return: 0 to hand over control
+ */
+
+int main(void)
+{
+	int num = 48;
+	while (num < 58)
+	{
+		putchar(num);
+		num++;
+	}
+
+	putchar('\n');
+	return (0);
+}
