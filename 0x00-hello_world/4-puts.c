cdd2f4579122f8bb1b51cfabe4c24bdedea543b1
Houssam <thisischchannel@gmail.com>
2023-06-16 05:00:38 -0700

    done writing 4-puts.c with succesful results

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	/* initializing str charachters */
+	char str[] = "Programming is like building a multilingual puzzle";
+
+	/* writing to stdout */
+	puts(str);
+
+	/* handing control over to processor */
+	return 0;
+}
