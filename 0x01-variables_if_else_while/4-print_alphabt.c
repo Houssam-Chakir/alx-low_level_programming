ac333832adfa7dc956d2ea018aca2886564d5686
Houssam <thisischchannel@gmail.com>
2023-06-16 16:06:32 -0700

    getting inifite loop but my code seems to be correct

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - listing all alphabets
+ *
+ * Description: using a loop to print
+ * all alphabets in lower case
+ * Return: 0 to hand over control
+ */
+
+int main(void)
+{
+        int index = 97;
+
+	while (index < 123)
+        {
+       	        if (index == 101 || index == 113)
+                {
+                        continue;
+                } else
+                {
+                putchar(index);
+                index++;
+                }
+        }
+        putchar('\n');
+
+        return (0);
+}
