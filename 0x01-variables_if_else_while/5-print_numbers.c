09a7c1874b25c56903347d8c6b0432ed87cade8a
Houssam <thisischchannel@gmail.com>
2023-06-16 16:29:59 -0700

    wrot and tested task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers
+ *
+ * Description: using loop to print base 10
+ * single digits until 9
+ * Return: 0 to hand over control
+ */
+
+int main(void)
+{
+	int nums = 0;
+
+	while (nums < 10)
+	{
+		printf("%d", nums);
+		nums++;
+	}
+
+	putchar('\n');
+	return (0);
+}
+	
