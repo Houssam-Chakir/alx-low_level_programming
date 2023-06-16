a0657ff6bf3b56793d9cc6152e253f27a0c88430
Houssam <thisischchannel@gmail.com>
2023-06-16 06:33:34 -0700

    wrote and tested task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing using printf
+ *
+ * Description: using printf function it will print desired output
+ * Return: it shoudl return 0 to hand over control
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
