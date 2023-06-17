af2e760cc2e5fd2b79d63474a261ffa65879a042
Houssam <thisischchannel@gmail.com>
2023-06-17 10:11:29 -0700

    wrote and tested task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints a string
+ *
+ * Description: using printf to put a string
+ * in the standard output
+ * Return: 1 as a returned value
+ */
+
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
