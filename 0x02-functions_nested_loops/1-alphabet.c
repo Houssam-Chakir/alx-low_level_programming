25eeaa7e5299afa2b51a89ff699459c2d38d9ce8
Houssam <thisischchannel@gmail.com>
2023-06-21 09:00:36 -0700

    fixed some betty styles

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,14 +4,14 @@
  * main - prints alphabets
  *
  * Description: funtion to print alphabets in lower case\
- * Return: a character
+ * Return: a character.
  */
 
 void print_alphabet(void)
 {
 	int index = 97;
 
-	while(index < 123)
+	while (index < 123)
 	{
 		_putchar(index);
 		index++;
