60b99613f974f20a51b2a5b43007e69bc5b52b54
Houssam <thisischchannel@gmail.com>
2023-06-23 14:18:18 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - testing printing function + or - int
+ *
+ * Return: 0 to hand over control
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
