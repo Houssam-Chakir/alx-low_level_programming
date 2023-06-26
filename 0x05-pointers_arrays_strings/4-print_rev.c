db406b11b65394cfdacc102a9073d77219f93506
Houssam <thisischchannel@gmail.com>
2023-06-26 14:17:41 -0700

    hot fixing

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,12 +11,13 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != 0)
+	while (*(s + i) != '\0')
 	{
 		i++;
 	}
 
-	while (i != -1)
+	s -= 1;
+	while (i >= 0)
 	{
 		_putchar(*(s + i));
 		i--;
