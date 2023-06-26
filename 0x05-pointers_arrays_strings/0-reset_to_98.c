6164c5ac00829a703e54745c6d653932f55aaad5
Houssam <thisischchannel@gmail.com>
2023-06-26 11:01:58 -0700

    fixed betty styles

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,12 +2,13 @@
 
 /**
  * reset_to_98 - resets n to 98
- * @&n: address
+ * @m: address
  *
  * using pointers to change a value
+ * Return: 0 to hand over control
  */
 
-int reset_to_98(int* m)
+int reset_to_98(int *m)
 {
 	*m = 98;
 	return (0);
