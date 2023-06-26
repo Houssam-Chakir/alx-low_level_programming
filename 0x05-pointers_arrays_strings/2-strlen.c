dcfb140e452ccbeee24ae89fefec5c3cf44cbb83
Houssam <thisischchannel@gmail.com>
2023-06-26 12:02:27 -0700

    rote and tested task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,14 @@
  * _strlen - gives lenght of a string
  * @s: string
  *
- * return: lenght of a string
+ * Return: lenght of a string
  */
 
 int _strlen(char *s)
 {
 	int index = 0;
 
-	while(*(s + index) != 0)
+	while (*(s + index) != 0)
 	{
 		index++;
 	}
