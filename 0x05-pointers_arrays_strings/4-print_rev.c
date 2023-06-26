54f2e11489e69fa5f756adb85edc0b8b4acd94d2
Houssam <thisischchannel@gmail.com>
2023-06-26 13:24:51 -0700

    wrote and tested task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,11 +11,13 @@ void print_rev(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != 0)
+	do
 	{
 		i++;
 	}
-	while (i > 0)
+	while (*(s + i) != 0);
+
+	while (i > -1)
 	{
 		_putchar(*(s + i));
 		i--;
