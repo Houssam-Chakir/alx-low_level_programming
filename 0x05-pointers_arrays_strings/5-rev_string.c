904630b9e20dfab76bf83d3ae9c7d94b948ea321
Houssam <thisischchannel@gmail.com>
2023-06-27 10:43:27 -0700

    wrote and tested task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "stdio.h"
+/**
+ * rev_string - reverses a string
+ * @*s: characters string
+ *
+ * Return: reversed string
+ */
+
+void rev_string(char *s)
+{
+	int len = 0, i = 0;
+	char tempo;
+
+	 while (*(s + i++))
+	{
+		len++;
+	}
+
+	i = len - 1;
+
+	while (i >= len / 2)
+	{
+		tempo = *(s + i);
+		s[i] = s[len - i - 1];
+		s[len - i - 1] = tempo;
+		i--;
+	}
+}
