b427b1b70a1fb7fd4f55a58920bcc4ea124e34e9
Houssam <thisischchannel@gmail.com>
2023-06-30 04:21:44 -0700

    wrote and tested task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcat - string concat'
+ * @dest: first part of the string
+ * @src: appended part of the string
+ *
+ * Return: a pointer to the resulting string dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int count = 0, index = 0;
+
+	while (dest[count])
+	{
+		count++;
+	}
+
+	while (src[index])
+	{
+		dest[count] = src[index];
+		count++;
+		index++;
+	}
+
+	return dest;
+}
