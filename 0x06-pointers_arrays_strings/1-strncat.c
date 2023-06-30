db7f90b925dfbb32ed2dfee727847de35582ee9d
Houssam <thisischchannel@gmail.com>
2023-06-30 05:34:01 -0700

    hot fixing

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,20 +12,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-        int count = 0, index = 0;
+	int count = 0, index = 0;
 
-        while (dest[count])
-        {
-                count++;
-        }
+	while (dest[count])
+	{
+		count++;
+	}
 
-        while (n > 0)
-        {
-                dest[count] = src[index];
-                count++;
-                index++;
+	while (n > 0 || n == index)
+	{
+		dest[count] = src[index];
+		count++;
+		index++;
 		n--;
-        }
+	}
 
-        return (dest);
+	return (dest);
 }
