fe7a47707fbb5e3e9199eadac572ad59959c6d1e
Houssam <thisischchannel@gmail.com>
2023-07-07 11:34:03 -0700

    task 1

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
+#!/bin/bash
 
+for file in *.c; do
+    if [ -f "$file" ]; then
+        filename="${file%.*}"
+        gcc -c -o "$filename.o" "$file"
+    fi
+done
+
+ar cr liball.a ./*.o
