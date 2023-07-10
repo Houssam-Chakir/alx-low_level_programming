eaa9bc3a7d34dce5b5608df10e65336930f5f8c2
Houssam <thisischchannel@gmail.com>
2023-07-10 06:17:22 -0700

    wrote and tested task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an array of chars
+ * @size: size of array
+ * @c: characters to be used
+ *
+ * Return: 0 for success
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array = NULL;
+
+	if (!(size <= 0))
+	{
+		array = malloc(size * sizeof(char));
+		while (size != 0)
+		{
+			array[size] = c;
+			size--;
+		}
+	}
+
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	return (array);
+}
