7f3aa2b269da1b5f3875900ef548287480144430
Houssam <thisischchannel@gmail.com>
2023-07-13 13:31:35 -0700

    task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - creates space in bytes
+ * @b: desired size
+ *
+ * Return: pointer to alloc space
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if ( ptr == NULL)
+	{
+		exit (98);
+	}
+	return (ptr);
+}
