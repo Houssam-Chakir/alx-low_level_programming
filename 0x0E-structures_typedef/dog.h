95d76845db358ea380100c12daddb48ed2fb7e12
Houssam <thisischchannel@gmail.com>
2023-07-17 13:23:55 -0700

    task 4 done

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,9 @@
 #ifndef DOG_GUARD
 #define DOG_GUARD
 
+int _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+
 /**
  * struct dog - info about a dog
  * @name: name
@@ -21,5 +24,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
