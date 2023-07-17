a0078975a646fb1c11bdd71effcbe9d870b8e5ce
Houssam <thisischchannel@gmail.com>
2023-07-17 09:45:21 -0700

    betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,20 @@
 #ifndef DOG_GUARD
 #define DOG_GUARD
 
+/**
+ * struct dog - info about a dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: its clear bruh
+ */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+	char *name; /**< the dog name */
+	float age; /**< dog age */
+	char *owner; /**< owner name */
+	/* ofc u can add more */
 };
 
 #endif
