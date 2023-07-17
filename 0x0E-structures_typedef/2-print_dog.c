8f4b0f2c624475bcf08adeb2061831ca6790db81
Houssam <thisischchannel@gmail.com>
2023-07-17 10:33:59 -0700

    task 2 fixing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,6 +13,10 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		d = malloc(sizeof(struct dog));
+	if (d->name == NULL)
+		d->name = "(ni)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
 
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
