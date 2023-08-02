e42333a721426df7418855ad7f3f3312d36265c0
Houssam <thisischchannel@gmail.com>
2023-08-01 23:56:07 -0700

    task103

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in the list
+ * @head: pointer to linked list
+ *
+ * Return: loop starting point address
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *x = head;
+	listint_t *y = head;
+
+	if (!head)
+		return (NULL);
+
+	while (x && y && y->next)
+	{
+		y = y->next->next;
+		x = x->next;
+		if (y == x)
+		{
+			x = head;
+			while (x != y)
+			{
+				x = x->next;
+				y = y->next;
+			}
+			return (y);
+		}
+	}
+
+	return (NULL);
+}
