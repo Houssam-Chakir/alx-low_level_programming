56ce89d233ab0929b1df4a388c7532cfebcd5529
Houssam Chakir <80906408+Houssam-Chakir@users.noreply.github.com>
2023-08-30 16:41:44 +0100

    Revert "Delete 0x0D-preprocessor directory"
    This reverts commit f6ae0aa90538ab40b120d16b2b2e460b0542f1ef.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/**
+ * main - prints file name
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef SUM_GUARD
+#define SUM_GUARD
+
+#define SUM(X, Y) (X + Y)
+
+#endif
